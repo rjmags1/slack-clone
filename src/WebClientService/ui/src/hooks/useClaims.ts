7c9c034dfa80cc3c59ba3553cffdbe6ebd9852f1
RJ Maganini <maganinirj@gmail.com>
2023-08-11 20:22:15 -0500

    Add background session checking

--- src/WebClientService/ui/src/hooks/useClaims.ts
+++ src/WebClientService/ui/src/hooks/useClaims.ts
@@ -2,6 +2,8 @@ import { useState, useEffect } from 'react'
 
 const SESSION_URL = '/bff/user'
 
+const THREE_MIN_MS = 180 * 1000
+
 export type Claim = {
     type: string
     value: string | number
@@ -13,9 +15,10 @@ function useClaims(): {
 } {
     const [claims, setClaims] = useState<Claim[] | null>(null)
     const [loading, setLoading] = useState<boolean>(true)
+    const [lastCheckedAt, setLastCheckedAt] = useState(Date.now())
 
     useEffect(() => {
-        const fetchData = async () => {
+        const getSession = async () => {
             var req = new Request(SESSION_URL, {
                 headers: new Headers({
                     'X-CSRF': '1',
@@ -25,9 +28,12 @@ function useClaims(): {
                 const res = await fetch(req)
                 if (res.ok) {
                     const data = await res.json()
-                    setClaims(data)
+                    if (claims === null) {
+                        setClaims(data)
+                    }
                     setLoading(false)
                 } else {
+                    setClaims(null)
                     setLoading(false)
                 }
             } catch (error) {
@@ -36,7 +42,16 @@ function useClaims(): {
             }
         }
 
-        fetchData()
+        const checkSession = async () => {
+            if (Date.now() - lastCheckedAt < THREE_MIN_MS) return
+
+            await getSession()
+            setLastCheckedAt(Date.now())
+        }
+
+        setInterval(checkSession, THREE_MIN_MS)
+        getSession()
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [])
 
     return { claims, loading }
