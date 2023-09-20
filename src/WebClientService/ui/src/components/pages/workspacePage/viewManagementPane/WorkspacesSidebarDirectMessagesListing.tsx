856a8232205f4763fe6d86c08c660c33984febce
RJ Maganini <maganinirj@gmail.com>
2023-09-20 17:37:01 -0500

    Static workspace page dm sidebar section

---
+++ src/WebClientService/ui/src/components/pages/workspacePage/viewManagementPane/WorkspacesSidebarDirectMessagesListing.tsx
@@ -0,0 +1,10 @@
+function WorkspaceSidebarDirectMessagesListing({ item }: any) {
+    return (
+        <div
+            className="px-2"
+            onClick={() => alert('not implemented')}
+        >{`${item.name}`}</div>
+    )
+}
+
+export default WorkspaceSidebarDirectMessagesListing
