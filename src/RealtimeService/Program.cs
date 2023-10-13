c005bad16a1d9bd2b32464fb7c6cd839c6ecd8a4
RJ Maganini <maganinirj@gmail.com>
2023-10-13 18:07:40 -0500

    Authenticate connections to realtime service

--- src/RealtimeService/Program.cs
+++ src/RealtimeService/Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.SignalR;
 using Microsoft.AspNetCore.Cors;
 using RealtimeService.Hubs;
+using Microsoft.AspNetCore.DataProtection;
+
+const string DATA_PROTECTION_KEY_PATH = "../../keys/data_protection_keys";
+const string DATA_PROTECTION_APPLICATION_NAME = "slack-clone";
 
 const string WEB_CLIENT_URL = "https://localhost:5003";
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services
+    .AddDataProtection()
+    .SetApplicationName(DATA_PROTECTION_APPLICATION_NAME)
+    .PersistKeysToFileSystem(new DirectoryInfo(DATA_PROTECTION_KEY_PATH));
 
 builder.Services.AddSignalR();
 
